de4bbabf0355982d92f2e4293af71585eb746263
xpengfei480s <1554198850@qq.com>
2020-02-16 18:07:31 +0800

    Initial commit

---
+++ src/main/java/com/xpffly/springcloud/SpringCloudEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.xpffly.springcloud;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer //启动服务注册中心，提供给其他应用进行会话
+@SpringBootApplication
+public class SpringCloudEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaServerApplication.class, args);
+    }
+
+}
